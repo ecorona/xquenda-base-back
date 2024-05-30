926d29f7f4839b8e4ed0328699dfc9c059cfa711
Erik Corona <ecorona@live.com>
2024-05-29 19:57:43 -0600

    Manejo de conexión y desconexion sin token

--- src/sockets-admin/sockets-admin.gateway.ts
+++ src/sockets-admin/sockets-admin.gateway.ts
@@ -8,11 +8,13 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
   WsResponse,
 } from '@nestjs/websockets';
 import { SocketsAdminGuard } from './sockets-admin.guard';
 import { Server } from 'socket.io';
 import { SocketUser } from './socket-user.dto';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({
   transports: ['websocket'],
@@ -21,17 +23,39 @@ import { SocketUser } from './socket-user.dto';
 export class SocketsAdminGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(private readonly authService: AuthService) {}
   @WebSocketServer()
   private server: Server;
   private readonly logger = new Logger(SocketsAdminGateway.name);
   afterInit() {
     this.logger.verbose('Sockets Gateway initialized');
   }
-  handleConnection(client: SocketUser, ...args: any[]) {
-    this.logger.log(
-      `Client connected: ${client.id}||${client.handshake.auth.token}||${JSON.stringify(args)}`,
-    );
+  handleConnection(client: SocketUser) {
+    if (!client.handshake.auth.token) {
+      client.emit('exception', new WsException('Token no proporcionado'));
+      client.disconnect();
+    }
+    this.authService
+      .validateToken(client.handshake.auth.token)
+      .then((user) => {
+        if (!user) {
+          client.emit('exception', new WsException('Usuario no encontrado'));
+          client.disconnect();
+        }
+        client.data = {
+          user,
+        };
+        this.logger.log(
+          `Client connected: ${client.id} - ${client.data.user.correo}`,
+        );
+      })
+      .catch((error) => {
+        this.logger.error(error);
+        client.emit('exception', new WsException('Token inválido'));
+        client.disconnect();
+      });
   }
+
   handleDisconnect(client: SocketUser) {
     this.logger.log(
       `Client disconnected: ${client.id} ${client.data.user?.correo}`,
@@ -46,11 +70,8 @@ export class SocketsAdminGateway
   private async handleCanales(
     @ConnectedSocket() client: SocketUser,
   ): Promise<WsResponse<Array<string>>> {
-    this.logger.log(`Client sent <canales>`);
     const canales = ['todos'];
-
     await client.join(canales);
-
     return {
       event: 'canales',
       data: canales,
